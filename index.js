ef368d0f7e4e3d89b5bc0e5c9b535777f8c667d8
gassll <lizakonopatskaya@gmail.com>
2025-03-31 17:58:07 +0300

    кнопка норм

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+// const btn = document.getElementById('modal__open');
+// const body = document.body;
+// const modal = document.querySelector('.modal');
+// const modalClose = document.querySelector('.modal__close');
+
+
+
+// btn.addEventListener('click', function() {
+// body.classList.add('active');
+// modal.classList.add('active');
+// });
+
+// modalClose.addEventListener('click', function() {
+//     body.classList.remove('active'); // просто удаляем класс
+//     modal.classList.remove('active'); // просто удаляем класс
+// });
+
+
+// modalClose.addEventListener('click', function() {
+//     body.classList.remove('active');
+//     modal.classList.remove('active');
+// });
+
 const btn = document.getElementById('modal__open');
 const body = document.body;
 const modal = document.querySelector('.modal');
-
-
-btn.addEventListener('click', function() {
-body.classList.add('active');
-modal.classList.add('active');
-});
-
-modalCloseBtn.addEventListener('click', function() {
-    body.classList.remove('active'); // просто удаляем класс
-    modal.classList.remove('active'); // просто удаляем класс
-});
-
 const modalClose = document.querySelector('.modal__close');
 
-modalClose.addEventListener('click', function() {
+btn.addEventListener('click', function () {
+    body.classList.add('active');
+    modal.classList.add('active');
+});
+
+modalClose.addEventListener('click', function () {
     body.classList.remove('active');
     modal.classList.remove('active');
 });
